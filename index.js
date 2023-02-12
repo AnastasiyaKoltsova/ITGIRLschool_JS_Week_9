866201ced601574a1d579993376228f30c32bf43
Anastasiya Koltsova <Koltsova_av@mail.ru>
2023-02-12 14:35:38 +0300

    update

---
+++ index.js
@@ -0,0 +1,35 @@
+const ub = 40000;
+const vok = 22000;
+console.log(ub + vok);
+// 62000
+
+console.log('Владивосток ' + 2000);
+// Владивосток 2000;
+
+let hours = (128 + 254 + 137 + 201) / 60;
+console.log(hours);
+// 12
+
+console.log(2**5);
+
+console.log('Нужно бежать со всех ног, чтобы только оставаться на месте, а чтобы куда-то попасть, надо бежать как минимум вдвое быстрее!');
+
+const greetings = "Привет";
+const name = "Кот";
+console.log(`${greetings}, ${name}`);
+// Привет, Кот
+
+let time;
+time = 34;
+console.log('Старт поездки. Осталось минут: ' + time);
+time = time - 15;
+console.log('Немного сторис в соцсетях. Осталось минут: ' + time);
+time = time - 10;
+console.log('Немного не новостей, но событий. Осталось минут: ' + time);
+time = 0;
+console.log('Вы приехали. Добро пожаловать в Москву');
+
+let fahrenheit = 451;
+let celsius = (fahrenheit - 32) / 1.8;
+console.log(`${fahrenheit} градуса по Фаренгейту — это ${celsius} градуса по Цельсию.`);
+// 451 градуса по Фаренгейту — это 232.77777777777777 градуса по Цельсию.
